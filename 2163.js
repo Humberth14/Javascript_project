3fb134e8e44d450ef341e8f079d4515e7bd83442
Humberth14 <humberto.sanchezto14@gmail.com>
2017-02-16 14:09:27 -0600

    Update 2163.js

--- 2163.js
+++ 2163.js
@@ -1,4 +1,4 @@
-var miCanvas=document.getElementById("miCanvas");
-var contexto=miCanvas.getContext("2d");
+var miCanvas2=document.getElementById("miCanvas2");
+var contexto=miCanvas2.getContext("2d");
 contexto.fillStyle="blue";
 contexto.fillRect(40,40,60,60);
