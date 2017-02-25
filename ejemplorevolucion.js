0ed1bdb0547da47cfed7d506898d75d2688d20dc
Humberth14 <humberto.sanchezto14@gmail.com>
2017-02-25 14:16:40 -0600

    Update ejemplorevolucion.js

--- ejemplorevolucion.js
+++ ejemplorevolucion.js
@@ -1,9 +1,10 @@
 var puntos = [];
-for ( var i = 0; i < 1; i ++ ) {
+for ( var i = 0; i < 50; i ++ ) {
     puntos.push( new THREE.Vector2(
                      Math.sin( i * 0.2 ) * 15 + 50,
                      ( i - 5 ) * 2 ) );
 }
+
 var forma = new THREE.LatheGeometry(puntos);
 
 var material = new THREE.MeshNormalMaterial();
@@ -20,4 +21,3 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
-
