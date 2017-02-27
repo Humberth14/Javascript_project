07180d87657dbe950a483b7f00a152fb2321888f
Humberth14 <humberto.sanchezto14@gmail.com>
2017-02-26 19:12:57 -0600

    Update ejemplorevolucion.js

--- ejemplorevolucion.js
+++ ejemplorevolucion.js
@@ -14,7 +14,7 @@ var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 250;
+camara.position.z = 50;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
