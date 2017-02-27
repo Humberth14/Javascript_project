e59e446c88488fafb448eb0771fc835c3acba981
Humberth14 <humberto.sanchezto14@gmail.com>
2017-02-26 19:15:13 -0600

    Update ejemplorevolucion.js

--- ejemplorevolucion.js
+++ ejemplorevolucion.js
@@ -9,12 +9,12 @@ var forma = new THREE.LatheGeometry(puntos);
 var material = new THREE.MeshNormalMaterial();
 
 var malla = new THREE.Mesh( forma, material );
-malla.rotateX( Math.PI );
+malla.rotateX( Math.PI/6 );
 var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 50;
+camara.position.z = 0;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
