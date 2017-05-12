ef298611c7b433a3dd23f22e860b5ddcdb29d9ae
Humberth14 <humberto.sanchezto14@gmail.com>
2017-05-11 20:44:15 -0500

    Update texturatablero1.js

--- texturatablero1.js
+++ texturatablero1.js
@@ -1,7 +1,7 @@
 function setup (){
 THREE.ImageUtils.crossOrigin = '';
 var textura =THREE.ImageUtils.loadTexture('Humberth14.github.io/espacio.jpg');
-var material = new THREE.MeshBasicMaterial({map: textura});
+var material = new THREE.MeshNormalMaterial({map: textura});
 var forma = new THREE.BoxGeometry(1,1,1);
 malla = new THREE.Mesh(forma, material);
 
@@ -9,14 +9,22 @@ malla = new THREE.Mesh(forma, material);
 escena = new THREE.Scene();
 escena.add(malla);
 
-  var camara = new THREE.PerspectiveCamera();
-camara.position.z = 40;
+camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
 
-var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95,
-                      window.innerHeight*.95 );
-document.body.appendChild( renderizador.domElement );
-renderizador.render( escena, camara );
+renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild(renderer.domElement);
 }
 
+function loop() {
+requestAnimationFrame(loop);
+
+malla.rotation.x += 0.01;
+malla.rotation.y += 0.01;
+
+renderer.render(escena, camara);
+}
+var camara, escena, renderer, malla;
 setup()
+loop();
