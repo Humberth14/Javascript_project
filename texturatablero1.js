87d6a9af450384ebd5d3919b1ce954b2dc760727
Humberth14 <humberto.sanchezto14@gmail.com>
2017-05-14 17:39:36 -0500

    Update texturatablero1.js

--- texturatablero1.js
+++ texturatablero1.js
@@ -10,7 +10,7 @@ escena = new THREE.Scene();
 escena.add(malla);
 
 camara = new THREE.PerspectiveCamera();
-camara.position.z = 125;
+camara.position.z = 101;
 
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95, window.innerHeight*.95);
