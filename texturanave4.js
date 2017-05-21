5077f3f0573ba834ea81bbe0e3a4438f50fea785
Humberth14 <humberto.sanchezto14@gmail.com>
2017-05-21 18:44:09 -0500

    Create texturanave4.js

---
+++ texturanave4.js
@@ -0,0 +1,45 @@
+function setup (){
+ THREE.ImageUtils.crossOrigin = '';
+  
+  var textura =THREE.ImageUtils.loadTexture('Humberth14.github.io/br.jpg');
+  var material = new THREE.MeshBasicMaterial({map: textura});
+var figura = new THREE.Shape();
+figura.moveTo(10, 10);
+figura.lineTo(15, -10);
+figura.lineTo(20, 0);
+figura.lineTo(20, 10);
+figura.lineTo(16, 5);
+figura.lineTo(15,20);
+figura.lineTo(14,5);
+figura.moveTo(10,10);
+
+
+var forma = new THREE.ExtrudeGeometry( figura,
+                                       {amount: 0.11} );
+
+
+
+
+malla = new THREE.Mesh( forma, material );
+
+escena = new THREE.Scene();
+escena.add(malla);
+
+camara = new THREE.PerspectiveCamera();
+camara.position.z = 100;
+
+renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild(renderer.domElement);
+}
+function loop() {
+requestAnimationFrame(loop);
+
+malla.rotation.x += 0;
+malla.rotation.y += 0;
+
+renderer.render(escena, camara);
+}
+var camara, escena, renderer, malla;
+setup()
+loop();
