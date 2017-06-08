3d82299b82ad5b663ffbea6ea04bab7098bfd074
Humberth14 <humberto.sanchezto14@gmail.com>
2017-06-07 23:17:06 -0500

    Create Asteroid.js

--- Asteroid.js
+++ Asteroid.js
@@ -73,15 +73,15 @@ function Asteroid(scene, scale, difficulty, viewportSize, time, audioContext, la
 		//	var bufferTexture = new THREE.TextureLoader();
 		//	bufferTexture.load("images/aste.jpg",function (texture){initTexture.asteroide = texture;});
 		//}
-		var textureAsteroid;
+		var textures=new Object();
 		var asteroidTexture = new THREE.TextureLoader();
-		var mainAsteroid=new THREE.Mesh();
 		asteroidTexture.load("images/aste.jpg",function(texture){
 			//var texture.asteroid=texture;
 			//textureAsteroid=texture;
 			//mainAsteroid = THREE.Line(asteroid1Geometry, new THREE.MeshBasicMaterial({map: asteroidTexture}), THREE.LineStrip);
-			 mainAsteroid = new THREE.Mesh(new THREE.SphereGeometry(3,20,20),new THREE.MeshBasicMaterial({map:texture}));
+			 textures.asteroid=texture;
 		});
+		var mainAsteroid = new THREE.Mesh(new THREE.SphereGeometry(3,20,20),new THREE.MeshBasicMaterial({map:textures.asteroid}));
 		//var asteroidMaterial = new THREE.MeshLambertMaterial({map: asteroidTexture});
 	    //create the asteroid
 	    
